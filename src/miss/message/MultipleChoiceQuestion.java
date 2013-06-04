3b61ff5009ad8f69f5384836d0b9d1851fa29a74
maciejs <maciejs@szeptem.pl>
2013-06-04 14:46:56 +0200

    zaimplementowano parsowanie odpowiedzi na pytanie wielokrotnego wyboru

--- src/miss/message/MultipleChoiceQuestion.java
+++ src/miss/message/MultipleChoiceQuestion.java
@@ -1,10 +1,12 @@
 package miss.message;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class MultipleChoiceQuestion extends ChoiceQuestion {
 
     public static final String WYBIERZ_WYBRANĄ_LICZBĘ_ELEMENTÓW_Z_LISTY_PODAJ_LICZBY_ODDZIELONE_PRZECINKAMI = "Wybierz wybraną liczbę elementów z listy (podaj liczby oddzielone przecinkami): ";
+    public static final String SEPERATOR = ",";
 
     private MultipleChoiceQuestion(){
     }
@@ -15,7 +17,12 @@ public class MultipleChoiceQuestion extends ChoiceQuestion {
 
     @Override
     public Message parseResponse(String response) {
-        return null;  //TODO
+        List<String> answer = new ArrayList<>();
+        for (String s : response.split(SEPERATOR)){
+            int i = Integer.parseInt(s) - 1;
+            answer.add(choices.get(i));
+        }
+        return new MultipleChoiceAnswer(answer.toArray(new String[answer.size()]));
     }
 
     @Override
